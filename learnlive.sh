f69dee7b9772c79d2faa14e1e9434339204a6350
welasco <26201510+Welasco@users.noreply.github.com>
2023-04-10 17:39:41 -0500

    add ingress

--- learnlive.sh
+++ learnlive.sh
@@ -21,3 +21,14 @@ k get -A gitrepositories.source.toolkit.fluxcd.io
 az k8s-configuration flux delete -g AKS -c vwsaks -n gitops-demo -t managedClusters
 
 #################################################################################################################################################
+
+#########################################################
+### Arc-enabled Kubernetes
+#########################################################
+# Arc-enabled Kubernetes connect
+az connectedk8s connect --name wld01-cluster --resource-group AKSHCILab --location easeus
+
+#az k8s-extension create -g ESLZ-SPOKE -n vws-app-config -c eslzakscluster --cluster-type managedClusters --extension-type=microsoft.flux
+az k8s-configuration flux create -g AKSHCILab -c wld01-cluster -n vws-app-config --namespace vws-app -t connectedClusters --scope cluster -u https://github.com/Welasco/testflux2.git --interval 2m --branch main --kustomization name=vws-app path=./vws-app prune=true sync_interval=2m
+az k8s-configuration flux update -g AKSHCILab -c wld01-cluster -n vws-app-config -t connectedClusters -u https://github.com/Welasco/testflux2.git --interval 2m --branch main  --kustomization name=vws-app path=./vws-app prune=true sync_interval=2m --kustomization name=vws-backend path=./vws-backend prune=true sync_interval=2m dependsOn=["vws-app"]
+#################################################################################################################################################
